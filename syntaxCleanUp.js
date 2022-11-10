f6183a2d73a908961cd4c9ea2b5a266de32115a5
Joshua Martin <ursa8minority@gmail.com>
2022-11-10 16:33:50 -0700

    Add syntax cleanup

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,38 @@
-var foods = {apples: 23, 
-  grapes: 507, 
-  eggs: 48}
+var foods = { 
+apples: 23, 
+grapes: 507, 
+eggs: 48
+}
 
-var person = { height: "6 ft", weight: "160 lbs" }
+var person = {
+height: "6ft", 
+weight: "160 lbs"
+}
 
-var post = { imageSrc: "./images/beach.png",
+var post = {
+imageSrc: "./images/beach.png",
+caption : "At the beach with my besties",
+timestamp : "4:37 PM August 13,2019",
+numberLikes : 0,
+comments: "Hello ya' old witch" 
+} 
 
 
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
+var post2 = {
 
-
-var post2 =
-{
       imageSrc: "./images/holiday-party.png",
       caption: "What a great holiday party omg",
       timestamp: "11:37 PM December 31, 2019",
       numberLikes: 13,
       comments: []
 }
+
+// Removed curly-bracket on line 1 that was before object.
+// Removed the equal sign on line 1 and replaced with colin marks . Proper syntax
+// Proper indentation on line 2, 3 , 4
+// Open curly-bracket after variable declared
+// Closed curly-bracket on line 5
+// Line 7 uses square bracket notation
+// Line 7 variable declared, used string notation within square bracket notation
+// Line 12-18 rearranged into an Array of objects.
+// line 22 remove open curly bracket and position it to line 21
